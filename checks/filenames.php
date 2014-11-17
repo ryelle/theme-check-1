769eea84df29254003002e07ea31e39ccfb1a302
ryelle <kelly.dwan@gmail.com>
2014-11-16 21:16:58 -0500

    Add - and _ to the hidden file regular expression, to catch dotfiles that begin with - or _.

--- checks/filenames.php
+++ checks/filenames.php
@@ -23,7 +23,7 @@ class File_Checks implements themecheck {
 				'project.properties'	=> __( 'NetBeans Project File', 'theme-check' ),
 				'project.xml'			=> __( 'NetBeans Project File', 'theme-check' ),
 				'\.kpf'					=> __( 'Komodo Project File', 'theme-check' ),
-				'^\.+[a-zA-Z0-9]'		=> __( 'Hidden Files or Folders', 'theme-check' ),
+				'^\.+[a-zA-Z0-9-_]'		=> __( 'Hidden Files or Folders', 'theme-check' ),
 				'php.ini'				=> __( 'PHP server settings file', 'theme-check' ),
 				'dwsync.xml'			=> __( 'Dreamweaver project file', 'theme-check' ),
 				'error_log'				=> __( 'PHP error log', 'theme-check' ),
